e99a4670a1dac2848f6ab7f96d2bfb5ab491bcd4
narcissujsk <1791121094@qq.com>
2018-04-17 10:03:50 +0800

    knight

---
+++ Chapter001/knight/src/main/java/sia/knights/BraveKnight.java
@@ -0,0 +1,19 @@
+package sia.knights;
+  
+public class BraveKnight implements Knight {
+
+  private Quest quest;
+
+  public BraveKnight(Quest quest) {
+    this.quest = quest;
+  }
+
+  public void embarkOnQuest() {
+	  System.out.println("embarkOnQuest");
+	  System.out.println("embarkOnQuest");
+    quest.embark();
+    System.out.println("embarkOnQuest");
+    System.out.println("embarkOnQuest");
+  }
+
+}
---
+++ Chapter001/knight/src/main/java/sia/knights/KnightMain.java
@@ -0,0 +1,24 @@
+package sia.knights;
+
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class KnightMain {
+
+	public static void main(String[] args) throws Exception {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("META-INF/spring/knight.xml");
+		Knight knight = context.getBean(Knight.class);
+		knight.embarkOnQuest();
+		context.close();
+	}
+
+	@Test
+	public void test() {
+
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("META-INF/spring/minstrel.xml");
+		Knight knight = context.getBean(Knight.class);
+		knight.embarkOnQuest();
+		context.close();
+
+	}
+}
