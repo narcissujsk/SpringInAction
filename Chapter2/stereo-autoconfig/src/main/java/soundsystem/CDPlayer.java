cb32fbddac80913882027a1f9cbc4582a12e0f61
narcissujsk <1791121094@qq.com>
2018-04-18 08:55:09 +0800

    ee

---
+++ Chapter2/stereo-autoconfig/src/main/java/soundsystem/CDPlayer.java
@@ -0,0 +1,18 @@
+package soundsystem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("cdplayer")
+public class CDPlayer implements MediaPlayer {
+  private CompactDisc cd;
+
+  @Autowired
+  public CDPlayer(CompactDisc cd) {
+    this.cd = cd;
+  }
+
+  public void play() {
+    cd.play();
+  }
+
+}
